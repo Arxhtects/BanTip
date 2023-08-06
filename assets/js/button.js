21d8b828dc72f743f1f49356d09d38b861008863
Archtects <scott.ch4mb3rs@gmail.com>
2023-08-06 23:16:03 +0100

    Add files via upload

--- assets/js/button.js
+++ assets/js/button.js
@@ -75,6 +75,13 @@ $(document).ready(function() {
         });
     }
 
+    if($('#siteTable').length) {
+        $('#siteTable').each(function() { 
+            let User = $(this).find('.author').text();
+            $(this).append('<div id="post_reply_target" class="tipButton reddit-the-best-one tip-best-reddit-old" data-target="key_post_author_main_reply_old_target"><span><strong>Tip ' + User + '</strong></span></div>');
+        });
+    }
+
     if($('div[data-testid="comment"]').length) {
         $('div[data-testid="comment"]').each(function() {
             $(this).append('<div class="loading reddit-worst-one tip-worst-reddit"></div>');
@@ -125,6 +132,23 @@ $(document).ready(function() {
                     targetButton.click();
                     targetKey.remove();
                 }, 100);
+            }, 100);
+        } else if(tipAmountTarget == 'key_post_author_main_reply_old_target') {
+            let commentTarget = targetKey.parent().next();
+            let targettextareaOld = commentTarget.find("form").first()
+            targettextareaOld.find("textarea").val("!ban " + request.greeting);
+            targettextareaOld.find("button[type=submit]").click();
+            targetKey.remove();
+        } else if(targetKey.attr("data-tag") == "old-reddit") {
+            let dataFullname = targetKey.parent().parent().attr("data-fullname");
+            var form = '<form action="#" class="usertext cloneable warn-on-unload" onsubmit="return post_form(this, \'comment\')" id="commentreply_' + dataFullname +'" style=""><input type="hidden" name="thing_id" value="'+ dataFullname +'"><div class="usertext-edit md-container" style="width: 500px;"><div class="md"><textarea rows="1" cols="1" name="text" class="" id="textarea_tip_target_reddit_'+ dataFullname +'" data-event-action="comment" data-type="link" style="width: 500px; height: 100px;"></textarea></div><div class="bottom-area"><span class="help-toggle toggle" style=""><a class="option active " href="#" tabindex="100" onclick="return toggle(this, helpon, helpoff)">formatting help</a><a class="option " href="#">hide help</a></span><a href="/help/contentpolicy" class="reddiquette" target="_blank" tabindex="100">content policy</a><span class="error CANT_REPLY field-parent" style="display:none"></span><span class="error TOO_LONG field-text" style="display:none"></span><span class="error RATELIMIT field-ratelimit" style="display:none"></span><span class="error NO_TEXT field-text" style="display:none"></span><span class="error SUBREDDIT_LINKING_DISALLOWED field-text" style="display:none"></span><span class="error SUBREDDIT_OUTBOUND_LINKING_DISALLOWED field-text" style="display:none"></span><span class="error USERNAME_LINKING_DISALLOWED field-text" style="display:none"></span><span class="error USERNAME_OUTBOUND_LINKING_DISALLOWED field-text" style="display:none"></span><span class="error TOO_OLD field-parent" style="display:none"></span><span class="error THREAD_LOCKED field-parent" style="display:none"></span><span class="error DELETED_COMMENT field-parent" style="display:none"></span><span class="error USER_BLOCKED field-parent" style="display:none"></span><span class="error USER_MUTED field-parent" style="display:none"></span><span class="error USER_BLOCKED_MESSAGE field-parent" style="display:none"></span><span class="error INVALID_USER field-parent" style="display:none"></span><span class="error MUTED_FROM_SUBREDDIT field-parent" style="display:none"></span><span class="error QUARANTINE_REQUIRES_VERIFICATION field-user" style="display:none"></span><span class="error TOO_MANY_COMMENTS field-text" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_REQUIRED field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_NOT_ALLOWED field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_BLACKLISTED_STRING field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_NOT_ALLOWED field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_REQUIRED field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_REQUIREMENT field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_REGEX_TIMEOUT field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_BODY_REGEX_REQUIREMENT field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_MAX_LENGTH field-body" style="display:none"></span><span class="error SUBMIT_VALIDATION_MIN_LENGTH field-body" style="display:none"></span><span class="error SOMETHING_IS_BROKEN field-parent" style="display:none"></span><span class="error placeholder field-body" style="display:none"></span><span class="error placeholder field-text" style="display:none"></span><div class="usertext-buttons"><button type="submit" id="submit_tip_target_' + dataFullname + '" onclick="" class="save">save</button><button type="button" onclick="return cancel_usertext(this);" class="cancel" style="">cancel</button><span class="status"></span></div></div><div class="markhelp" style="display:none"><p></p><p>reddit uses a slightly-customized version of <a href="http://daringfireball.net/projects/markdown/syntax">Markdown</a> for formatting. See below for some basics, or check <a href="/wiki/commenting">the commenting wiki page</a> for more detailed help and solutions to common issues.</p> <p></p><table class="md"><tbody><tr style="background-color: #ffff99; text-align: center"><td><em>you type:</em></td><td><em>you see:</em></td></tr><tr><td>*italics*</td><td><em>italics</em></td></tr><tr><td>**bold**</td><td><b>bold</b></td></tr><tr><td>[reddit!](https://reddit.com)</td><td><a href="https://reddit.com">reddit!</a></td></tr><tr><td>* item 1<br>* item 2<br>* item 3</td><td><ul><li>item 1</li><li>item 2</li><li>item 3</li></ul></td></tr><tr><td>&gt; quoted text</td><td><blockquote>quoted text</blockquote></td></tr><tr><td>Lines starting with four spaces<br>are treated like code:<br><br><span class="spaces">&nbsp;&nbsp;&nbsp;&nbsp;</span>if 1 * 2 &lt; 3:<br><span class="spaces">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>print "hello, world!"<br></td><td>Lines starting with four spaces<br>are treated like code:<br><pre>if 1 * 2 &lt; 3:<br>&nbsp;&nbsp;&nbsp;&nbsp;print "hello, world!"</pre></td></tr><tr><td>~~strikethrough~~</td><td><strike>strikethrough</strike></td></tr><tr><td>super^script</td><td>super<sup>script</sup></td></tr></tbody></table></div></div></form>'
+            let childTarget = targetKey.parent().next();
+            childTarget.append(form);
+            setTimeout(() => {
+                let oldreddittextareaTarget = $("#textarea_tip_target_reddit_" + dataFullname);
+                oldreddittextareaTarget.val("!ban " + request.greeting);
+                $("#submit_tip_target_" + dataFullname).click();
+                targetKey.remove();
             }, 100);
         } else {
             console.log(targetKey);
@@ -190,7 +214,11 @@ $(document).ready(function() {
       //OLD REDDIT
       [...document.querySelectorAll('.reddit-the-best-one')].forEach(function(item) { //Do not ask me why this has 3 dots. i dont know, it wont work without them, i dont ask questions.
         item.addEventListener('click', function() {
+            let highlightText;
             chrome.runtime.sendMessage({content: windowSize, type: "OpenPopup"});
+            tipAmountTarget = $(this).attr("data-target");
+            highlightText = $(this).parent();
+            highlightText.append('<p id="' + tipAmountTarget + '" class="_hidden_tip_chrome_addition_tip" data-tag="old-reddit"></p>') //have to keep the space
         });
     });
 
